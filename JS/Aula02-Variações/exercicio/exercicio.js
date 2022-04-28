769ac2495f4246db92780224eaf0cfe5a8d3d290
Joyce <joyce.emailprofissional@gmail.com>
2022-04-27 21:46:34 -0300

    aula16autualizado

---
+++ JS/Aula02-Variações/exercicio/exercicio.js
@@ -0,0 +1,9 @@
+let nome = "Joyce";
+let altura = 1.65;
+let peso = 72;
+let imc = peso / altura**2;
+imc = imc.toFixed(1)
+
+ console.log (`${nome} possui peso ${peso} e altura ${altura}, seu imc é ${imc}` )
+
+ alert(`${nome} possui peso ${peso} e altura ${altura}, seu imc é ${imc}`)
