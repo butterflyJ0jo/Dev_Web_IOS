f1001366e7c6c6412d7e24adc8635ec82ea24d6d
butterflyJojo <joyce.emailprofissional@gmail.com>
2022-05-03 20:18:33 -0300

    Js3-completo

--- JS/Aula04-Arrays/exercicio/main.js
+++ JS/Aula04-Arrays/exercicio/main.js
@@ -25,3 +25,22 @@ const matrix = [
 // Acessando elementos diversos dos arrays
 console.log(`Acessa a primeira linha: ${matrix[0]}`);
 console.log(`Acessa o segundo elemento: ${matrix[0][1]}`);
+
+// Alterando um valor do array
+console.clear();
+let moedas = ['Real', 'Dólar', 'Coroa', 'Peso'];
+console.log(`Antes: ${moedas}`);
+moedas[2] = 'Yen';
+console.log(`Depois: ${moedas}`);
+let matrix1 = [
+[1, 2, 3],
+[4, 5, 6],
+];
+console.log('Antes:');
+console.log(matrix1);
+
+// Alterando um valor do array
+// console.clear();
+matrix1[0][2] = -10;
+console.log('Depois:');
+console.log(matrix1);
